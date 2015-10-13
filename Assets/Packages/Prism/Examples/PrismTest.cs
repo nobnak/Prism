4b45c7fb15cbcb2b2d9cf09148d70866d0c4eda1
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-10-13 14:47:10 +0900

    git@github.com:nobnak/Prism.git

---
+++ Assets/Packages/Prism/Examples/PrismTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrismTest : MonoBehaviour {
+	public int count;
+	public float radius;
+	public Vector3 angularSpeed;
+	public GameObject particlefab;
+
+	void Start () {
+		for (var i = 0; i < count; i++) {
+			var pos = radius * Random.insideUnitSphere;
+			var go = (GameObject)Instantiate(particlefab, pos, Random.rotationUniform);
+			go.transform.SetParent(transform, false);
+		}
+	}
+
+	void Update() {
+		transform.localRotation *= Quaternion.Euler (Time.deltaTime * angularSpeed);
+	}
+}
---
+++ Assets/Packages/Prism/Prism.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PrismSystem {
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(Camera))]
+	public class Prism : MonoBehaviour {
+		public const float FULL_ROTATION = 360f;
+
+		public int cameraCount = 4;
+		public CameraParams overrideCameraParams;
+
+		RenderTexture _outputTex;
+		Camera _targetCamera;
+		Camera[] _cameras;
+		 
+		void OnEnable() {
+			_targetCamera = GetComponent<Camera> ();
+			_targetCamera.clearFlags = CameraClearFlags.Nothing;
+			_targetCamera.cullingMask = 0;
+
+			CheckInit();
+			Activate (true);
+		}
+		void OnDisable() {
+			Activate (false);
+		}
+		void OnDestroy() {
+			Release ();
+		}
+		void Update() {
+			CheckInit ();
+		}
+
+		void CheckInit() {
+			if (_cameras == null || _cameras.Length != cameraCount) {
+				Release ();
+				_cameras = new Camera[cameraCount];
+				for (var i = 0; i < cameraCount; i++) {
+					var name = string.Format ("{0} ({1})", _targetCamera.gameObject.name, i);
+					var go = new GameObject (name);
+					_cameras [i] = go.AddComponent<Camera> ();
+					go.hideFlags = HideFlags.DontSave;
+				}
+			}
+
+			var aspect = _targetCamera.aspect;
+			var theta_h = _targetCamera.fieldOfView;
+			var theta_w = FULL_ROTATION / cameraCount;
+			var w = 1f / cameraCount;
+			var a = w / (2f * Mathf.Tan(0.5f * theta_w * Mathf.Deg2Rad));
+			var h = 2f * a * Mathf.Tan(0.5f * theta_h * Mathf.Deg2Rad) * aspect;
+
+			var offsety = 0.5f * (1f - h);
+			for (var i = 0; i < cameraCount; i++) {
+				var c = _cameras[i];
+				c.CopyFrom(_targetCamera);
+				overrideCameraParams.Save(c);
+				c.orthographic = false;
+				c.rect = new Rect(w * i, offsety, w, h);
+
+				c.transform.SetParent(transform, false);
+				c.transform.localPosition = Vector3.zero;
+				c.transform.localRotation = Quaternion.Euler(0f, i * theta_w, 0f);
+			}
+		}
+		void Activate(bool active) {
+			if (_cameras == null)
+				return;
+			foreach (var c in _cameras) {
+				if (c == null)
+					continue;
+				c.gameObject.SetActive(active);
+			}
+		}
+		void Release() {
+			if (_cameras == null)
+				return;
+
+			foreach (var c in _cameras) {
+				if (c != null) {
+					var go = c.gameObject;
+					DestroyModal(c);
+					DestroyModal(go);
+				}
+			}
+			_cameras = null;
+		}
+		void DestroyModal(Object c) {
+			if (c == null)
+				return;
+			if (Application.isPlaying)
+				Destroy (c);
+			else
+				DestroyImmediate (c, true);
+		}
+
+		[System.Serializable]
+		public class CameraParams {
+			public CameraClearFlags clearFlags;
+			public Color backgroundColor;
+			public LayerMask cullingMasks;
+
+			public void Load(Camera c) {
+				clearFlags = c.clearFlags;
+				backgroundColor = c.backgroundColor;
+				cullingMasks = c.cullingMask;
+			}
+			public void Save(Camera c) {
+				c.clearFlags = clearFlags;
+				c.backgroundColor = backgroundColor;
+				c.cullingMask = cullingMasks;
+			}
+		}
+	}
+}
